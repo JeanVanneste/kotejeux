a130d81b16053b74c798bcd4727b09067a4d8850
JeanVanneste <jean.vanneste@student.vinci.be>
2018-10-05 13:45:16 +0200

    test template

--- src/Controller/LuckyNumberController.php
+++ src/Controller/LuckyNumberController.php
@@ -4,15 +4,19 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class LuckyNumberController extends AbstractController
 {
+    /**
+    *@Route("/lucky/number");
+    **/
+
     public function number(){
          $number = random_int(0,100);
 
-         return new Response(
-             '<html><body>Lucky number: '.$number.'</body></html>'
-         );
+         return $this->render('lucky/number.html.twig', ['number'=> $number,
+     ]);
     }
     /*
      public function index()
