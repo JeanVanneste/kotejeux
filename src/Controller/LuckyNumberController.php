25cc22c149421320d5e35a64d7e8310082f26270
JeanVanneste <jean.vanneste@student.vinci.be>
2018-10-05 12:00:51 +0200

    test routing

--- src/Controller/LuckyNumberController.php
+++ src/Controller/LuckyNumberController.php
@@ -3,15 +3,10 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
 class LuckyNumberController extends AbstractController
 {
-    /**
-     * @Route("/lucky/number", name="lucky_number")
-     */
-
     public function number(){
          $number = random_int(0,100);
 
