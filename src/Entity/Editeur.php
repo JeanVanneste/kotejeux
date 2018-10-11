74b66dfe9f16e0c8cafb19c1e3a43963beeee636
JeanVanneste <jean.vanneste@student.vinci.be>
2018-10-11 15:14:35 +0200

    Generate Editeur entity

---
+++ src/Entity/Editeur.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\EditeurRepository")
+ */
+class Editeur
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $nationalite;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $creationYear;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $logo;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getNationalite(): ?string
+    {
+        return $this->nationalite;
+    }
+
+    public function setNationalite(?string $nationalite): self
+    {
+        $this->nationalite = $nationalite;
+
+        return $this;
+    }
+
+    public function getCreationYear(): ?int
+    {
+        return $this->creationYear;
+    }
+
+    public function setCreationYear(?int $creationYear): self
+    {
+        $this->creationYear = $creationYear;
+
+        return $this;
+    }
+
+    public function getLogo(): ?string
+    {
+        return $this->logo;
+    }
+
+    public function setLogo(?string $logo): self
+    {
+        $this->logo = $logo;
+
+        return $this;
+    }
+}
