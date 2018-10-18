a3ecfed63bf0b9ffd587bd449f5d369c39c7d9d5
JeanVanneste <jean.vanneste@student.vinci.be>
2018-10-18 13:31:05 +0200

    Creation of JeuController

---
+++ src/Controller/JeuController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Editeur;
+
+class JeuController extends AbstractController
+{
+    /**
+     * @Route("/jeu", name="jeu")
+     */
+    public function index()
+    {
+        return $this->render('jeu/index.html.twig', [
+            'controller_name' => 'JeuController',
+        ]);
+    }
+}
