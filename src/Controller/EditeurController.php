bcb1750bf46e60a62ca50182c592b7a4d2512874
JeanVanneste <jean.vanneste@student.vinci.be>
2018-11-15 12:49:05 +0100

    Indentation

--- src/Controller/EditeurController.php
+++ src/Controller/EditeurController.php
@@ -11,61 +11,69 @@ use App\Form\EditeurType;
 
 class EditeurController extends AbstractController
 {
-    /**
-     * @Route("/editeur", name="editeur")
-     */
-    public function index()
-    {
-        return $this->render('editeur/index.html.twig', [
-            'controller_name' => 'editeurController',
-        ]);
+  /**
+  * @Route("/editeur", name="editeur")
+  */
+  public function index()
+  {
+    return $this->render('editeur/index.html.twig', [
+      'controller_name' => 'editeurController',
+    ]);
+  }
+
+  /**
+  * @Route("/editeur/add", name="editeurAdd")
+  */
+  public function new(Request $request)
+  {
+    $editeur = new Editeur();
+
+    $form = $this->createForm(EditeurType::class, $editeur);
+
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+      $entityManager = $this->getDoctrine()->getManager();
+
+      $editeur = $form->getData();
+
+      $entityManager->persist($editeur);
+      $entityManager->flush();
+
+      return new Response(
+        'Saved new editeur with id : '.$editeur->getId());
+      }
+
+      return $this->render('editeur/add.html.twig', array(
+        'form' => $form->createView(),
+      ));
     }
 
     /**
-    * @Route("/editeur/add", name="editeurAdd")
+    * @Route("/editeur/{id}", name="editeurShow")
     */
-    public function new(Request $request)
-    {
-        $editeur = new Editeur();
-
-        $form = $this->createForm(EditeurType::class, $editeur);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-
-            $editeur = $form->getData();
-
-            $entityManager->persist($editeur);
-            $entityManager->flush();
-
-            return new Response(
-                'Saved new editeur with id : '.$editeur->getId());
-        }
-
-        return $this->render('editeur/add.html.twig', array(
-            'form' => $form->createView(),
-        ));
-    }
-
-    /**
-     * @Route("/editeur/{id}", name="editeurShow")
-     */
     public function show($id)
     {
-        $editeur = $this->getDoctrine()->getRepository(Editeur::class)->find($id);
+      $editeur = $this->getDoctrine()->getRepository(Editeur::class)->find($id);
 
-        if (!$editeur) {
-            throw $this->createNotFoundException(
-                'No editeur found for id '.$id
-            );
-        }
+      if (!$editeur) {
+        throw $this->createNotFoundException(
+          'No editeur found for id '.$id
+        );
+      }
 
-        return $this->render('editeur/view.html.twig', [
-            'editeur_name' => $editeur->getName(),
-            'editeur_nationalite' => $editeur->getNationalite(),
-            'editeur_creation' => $editeur->getCreationYear(),
-        ]);
+      return $this->render('editeur/view.html.twig', [
+        'editeur_name' => $editeur->getName(),
+        'editeur_nationalite' => $editeur->getNationalite(),
+        'editeur_creation' => $editeur->getCreationYear(),
+      ]);
     }
-}
+
+    /**
+    * @Route("/editeur/{id}", name="editeurUpdate")
+    */
+    public function update($id)
+    {
+      $editeur = $this->getDoctrine()->getRepository(Editeur::class)->find($id);
+    }
+  }
